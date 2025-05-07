254a9854279760f254589fd39d9dcac4c8a30486
gopal <gopal1409@gmail.com>
2025-05-07 15:28:19 -0400

    terraform ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 #i want to create an ec2 instance using terraform
-data "aws_ami" "ubuntu" {
-  most_recent = true
+data "aws_ami" "hrms" {
+  most_recent = true #look for the recent version of the os
 
   filter {
     name   = "name"
@@ -9,14 +9,15 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "virtualization-type"
-    values = ["hvm"]
+    values = ["hvm"] #this is the virtualziation type
   }
 
   owners = ["099720109477"] # Canonical
 }
+#the above block will gather the ami information
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.hrms.id #this is the combination of data block+amiid
   instance_type = "t3.micro"
 
   tags = {
