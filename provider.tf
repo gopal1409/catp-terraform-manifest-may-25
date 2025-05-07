5640f3e97ae55ce16aea25a1d3104ac493afa612
gopal <gopal1409@gmail.com>
2025-05-07 14:31:08 -0400

    provider block in terraform

---
+++ provider.tf
@@ -0,0 +1,16 @@
+#this provider block is used for declare the cloud service provider configuration
+#the code is already build you just need to use the same 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
